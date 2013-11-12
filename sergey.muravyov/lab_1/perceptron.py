a015cb77ee752a1fb476afbd567f769286268072
seregamuravvyov <mursmail@gmail.com>
2013-11-12 18:42:17 +0400

    Fixing some bad bugs

--- sergey.muravyov/lab_1/perceptron.py
+++ sergey.muravyov/lab_1/perceptron.py
@@ -5,37 +5,40 @@ try:
 except ImportError:
     from urllib import urlopen
 
+def shuffle(x, y):
+    tmp = list(zip(x, y))
+    np.random.shuffle(tmp)
+    x_new, y_new = [], []
+    for i in range(len(tmp)):
+        x_new.append(tmp[i][0])
+        y_new.append(tmp[i][1])
+    return x_new, y_new
+
 def get_data():
     x, y = [], []
     file = urlopen("http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data")
     for line in file.readlines():
         fields = line.decode('utf-8').strip().split(',')
-        y.append(1 if fields[1] == 'M' else 0)
+        y.append(1 if fields[1] == 'M' else -1)
         num = [float(i) for i in fields[2:]]
         num.insert(0, 1.0)
+        num = np.array(num)
         x.append(num)
     file.close()
-    return x, y
+    return shuffle(x, y)
 
 def linear_regression(xs, ys):
     return np.dot(np.linalg.pinv(xs), ys)
 
 def misclassify(samples, w):
-    result = []
-    for (i, j) in samples:
-        if (classify(w, i) != j):
-            result.append((i, j))
+    result = [(x, y) for (x, y) in samples if not equal(classify(w, x), y)]
     return result
 
 def equal(x1, x2):
-    return abs(x1 - x2) < 0.001
+    return abs(x1 - x2) < 1e-3
 
 def classify(w, x):
-    prod = np.inner(w, x)
-    if prod > 0:
-        return 1
-    else:
-        return -1
+    return 1 if np.inner(w, x) > 0 else -1
 
 def train_perceptron(x, y, iteractions=1000):
     w = linear_regression(x, y)
@@ -51,8 +54,8 @@ def train_perceptron(x, y, iteractions=1000):
         if best_result == 0:
             break
 
-        x, y = choice(mc)
-        w += y * np.array(x)
+        x1, y1 = choice(mc)
+        w += y1 * x1
 
     return best_w
 
@@ -60,11 +63,10 @@ def main(test_fraction):
     x, y = get_data()
     test_size = int(len(x) * test_fraction)
 
-
-    w = train_perceptron(x[:-test_size], y[:-test_size], 1000)
+    w = train_perceptron(x[test_size:], y[test_size:], 1000)
 
     stats = {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0}
-    for i, j in zip(x[-test_size:], y[-test_size:]):
+    for (i, j) in zip(x[:test_size], y[:test_size]):
         yc = classify(w, i)
         if j == -1:
             key = 'tn' if yc == -1 else 'fp'
@@ -74,11 +76,11 @@ def main(test_fraction):
             stats[key] += 1
 
 
-    prec = (stats['tp']) / (stats['tp'] + stats['fp'])
-    recall = (stats['tp']) / ( stats['tp'] + stats['fn'])
-    F1 = 2 * prec * recall / (prec + recall)
-
-    print('precision = %6.2f\nrecall = %6.2f\nF_1 = %f' % (100 * prec, 100 * recall, F1))
+    prec = (stats['tp']) / (stats['tp'] + stats['fp']) if (stats['tp'] + stats['fp']) is not 0 else 0
+    recall = (stats['tp']) / ( stats['tp'] + stats['fn']) if (stats['tp'] + stats['fp']) is not 0 else 0
+    F1 = 2 * prec * recall / (prec + recall) if (prec + recall) is not 0 else 0
+   ## print(w)
+    print('precision = %6.5f\nrecall = %6.5f\nF_1 = %f' % (100 * prec, 100 * recall, F1))
 
 if __name__ == "__main__":
     main(0.1)
--- sergey.muravyov/lab_1/svm.py
+++ sergey.muravyov/lab_1/svm.py
@@ -7,18 +7,27 @@ try:
 except ImportError:
     from urllib import urlopen
 
+def shuffle(x, y):
+    tmp = list(zip(x, y))
+    np.random.shuffle(tmp)
+    x_new, y_new = [], []
+    for i in range(len(tmp)):
+        x_new.append(tmp[i][0])
+        y_new.append(tmp[i][1])
+    return x_new, y_new
 
 def get_data():
     x, y = [], []
     file = urlopen("http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data")
     for line in file.readlines():
         fields = line.decode('utf-8').strip().split(',')
-        y.append(1 if fields[1] == 'M' else 0)
+        y.append(1 if fields[1] == 'M' else -1)
         num = [float(i) for i in fields[2:]]
         num.insert(0, 1.0)
         x.append(num)
     file.close()
-    return x, y
+    return shuffle(x, y)
+
 
 def split(ls, parts):
     n = len(ls)
@@ -43,10 +52,10 @@ def unzip(*args):
 def best_regularization(x, y, parts=5):
     part_size = int(len(x) / parts)
     best_error = 1
-    best_C = 1
+    best_C = 0
 
-    for deg in range(30):
-        const = (1.0 / 3.0) ** (deg)
+    for d in range(200):
+        const = (1.0 / 9.0) ** (d)
         curr_error = 0
 
         for i in range(parts):
@@ -56,11 +65,10 @@ def best_regularization(x, y, parts=5):
             training_set_x = x[:i * part_size] + x[(i + 1) * part_size:]
             training_set_y = y[:i * part_size] + y[(i + 1) * part_size:]
 
-            svmReg = svm.LinearSVC(C=const)
-            svmReg.fit(training_set_x, training_set_y)
+            model = svm.LinearSVC(C=const)
+            model.fit(training_set_x, training_set_y)
 
-
-            prediction = svmReg.predict(check_set_x)
+            prediction = model.predict(check_set_x)
 
             misclassified = 0
             for j in range(len(check_set_x)):
@@ -68,7 +76,7 @@ def best_regularization(x, y, parts=5):
                     misclassified += 1
             curr_error += misclassified / len(check_set_x) / parts
 
-        if curr_error < best_error:
+        if curr_error < best_error or best_C == 0:
             best_error = curr_error
             best_C = const
 
@@ -77,10 +85,10 @@ def best_regularization(x, y, parts=5):
 def main():
     xs, ys = get_data()
     test_size = int(len(xs) * 0.1)
-    train_xs = xs[:-test_size]
-    train_ys = ys[:-test_size]
-    
-    C = best_regularization(train_xs, train_ys)
+    train_xs = xs[:test_size]
+    train_ys = ys[:test_size]
+
+    C = best_regularization(train_xs, train_ys, 10)
 
     model = svm.LinearSVC(C=C)
     model.fit(train_xs, train_ys)
@@ -89,8 +97,8 @@ def main():
     for x, x_pred in zip(list(ys[-test_size:]), list(model.predict(xs[-test_size:]))):
         if x != x_pred:
             errors += 1
-    print("error on test set: %6.2f%%" % (100 * errors / test_size))
-    print("regularization constant is 1e-%d" % round(log(C, 0.1)))
+    print("error on test set: %6.5f%%" % (100 * errors / test_size))
+    print("regularization constant is %6.2f" % C)
 
 if __name__ == "__main__":
     main()

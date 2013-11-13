d00d724b66633e2d270b63754fe8c683c8668ef3
Malova Anna <an.forgotenn@gmail.com>
2013-11-13 18:05:21 +0400

    add lab2 svm

--- anna.malova/lab1/perceptron.py
+++ anna.malova/lab1/perceptron.py
@@ -1,9 +1,5 @@
+from data import *
 import numpy as np
-
-class Vector:
-    def __init__(self, data, label):
-        self.data = data
-        self.label = label
 
 class Perceptron:
     def __init__(self, vec, steps):
@@ -18,44 +14,12 @@ class Perceptron:
     def get_label(self, v):
         return np.sign(np.dot(self.theta.T, v.data))
 
-    def get_training_error(self, vec):
-        return np.count_nonzero([self.get_label(v.data) - v.label] for v in vec) / len(vec)
 
-    def get_accuracy(self, vec):
-        tp = fp = fn = tn = 0
-        for v in vec:
-            if self.get_label(v) == v.label:
-                if v.label == 1:
-                    tp += 1
-                else:
-                    tn += 1
-            else:
-                if v.label == 1:
-                    fn += 1
-                else:
-                    fp += 1
+def main(train_fraction, steps):
+    train, test = load_data(train_fraction)
+    classifier = Perceptron(train, steps)
 
-        precision = tp / (tp + fp)
-        accuracy = (tp + tn) / (tp + tn + fp + fn)
+    print_results(classifier, test)
 
-        return (precision, accuracy)
-
-
-def read_data(train_percent):
-    inf = open('wdbc.data')
-    ss = inf.readlines()
-    vecs = np.array([Vector(np.array(list(map(float, s.split(',') [2:]))), 1 if s.split(',')[1] == 'M' else -1) for s in ss])
-    perm = np.random.permutation(vecs.size)
-    train_size = int(train_percent * vecs.size)
-    tr, t = perm[:train_size], perm[train_size:]
-    inf.close()
-
-    return (vecs[tr], vecs[t])
-
-train, test = read_data(0.3)
-classifier = Perceptron(train, 1000)
-
-print("Training error = " + str(classifier.get_training_error(test)))
-ac = classifier.get_accuracy(test)
-print("Precision = " + str(ac[0]))
-print("Accuracy = " + str(ac[1]))
+if __name__ == "__main__":
+    main(0.3, 1000)
--- anna.malova/lab1/svm.py
+++ anna.malova/lab1/svm.py
@@ -2,23 +2,32 @@ from sklearn import svm
 from data import *
 
 def train_SVM(train_data):
-	best_score = None, best_C = None, best_classifier = None
-	for C in range(1000):
-		classifier = svm.LinearSVC(C = C)
-		train, test = split_data(train_data, 0.5)
-		classifier.fit([v.data for v in train], [v.label for v in train])
-		score = score([v.data for v in test], [v.label for v in test])
-		if best_score == None or score > best_score:
-			best_score = score
-			best_C = C
-			best_classifier = classifier
-	return (best_classifier, best_C, best_score)		
-		
+    best_score = None
+    best_C = None
+    best_classifier = None
+
+    for C in 2. ** np.arange(-3, 15):
+        classifier = svm.LinearSVC(C=C)
+        train, test = split_data(train_data, 0.5)
+        datas = np.array([v.data for v in train])
+        labels = np.array([v.label for v in train])
+        classifier.fit(datas, labels)
+        score = classifier.score([v.data for v in test], [v.label for v in test])
+        if best_score == None or score > best_score:
+            best_score = score
+            best_C = C
+            best_classifier = classifier
+    return (best_classifier, best_C, best_score)
+
+
 def main(train_fraction):
-	train, test = load_data(train_fraction)
-	lin_svm = train_SVM(train)
-	print("Best C = " + str(lin_svm[2]))
-	print("Score on train data = " + str(lin_svm[1]))
-	score = lin_svm[0].score([v.data for v in test], [v.label for v in test])
-	print("Score on test data = " + str(score))
-	
+    train, test = load_data(train_fraction)
+    lin_svm = train_SVM(train)
+    print("Best C = " + str(lin_svm[1]))
+    print("Score on train data = " + str(lin_svm[2]))
+    score = lin_svm[0].score([v.data for v in test], [v.label for v in test])
+    print("Score on test data = " + str(score))
+
+if __name__ == "__main__":
+    main(0.7)
+

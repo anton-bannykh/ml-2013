6be185403c7e551113eb3451ad24c3523ac5f183
valentinka <valentinka@valentinka-Lenovo-IdeaPad-Z500.(none)>
2013-11-15 21:51:22 +0400

    percepton is work now)

--- Valentina.Vorobyeva/learn.py
+++ Valentina.Vorobyeva/learn.py
@@ -5,26 +5,27 @@ import random
 perc = []
 
 
-def exac_learn(x):
+def exac_learn((y, x)):
     global perc
     eta = 0.05
-    if x[0] == 'M':
-        t = 1
-    else:
-        t = -1
-    x[0] = 1
-    for i in range(len(x)):
-        x[i] = float(x[i])
-
-    o = 0
+    o = 0.
     for i in range(len(x)):
         o += perc[i] * x[i]
     if o >= 0:
-        o = 1
+        o = 1.
     else:
-        o = -1
-    for i in range(len(x)):
-        perc[i] += eta * (t - o) * x[i]
+        o = -1.
+    if y - o == 0:
+        return False
+    else:
+        for i in range(len(x)):
+            perc[i] += eta * (y - o) * x[i]
+        return True
+
+
+def init(length):
+    for i in range(length):
+        perc.append((random.randrange(0, 5)) / 500.0)
 
 
 def learn():
@@ -32,14 +33,20 @@ def learn():
     datas = []
     for line in f:
         data = line.split(',')
-        del data[0]
+        if data[1] == 'M':
+            y = 1.
+        else:
+            y = -1.
+        data = (y, [1.] + [float(x) for x in data[2:]])
         datas.append(data)
-
-    for i in range(len(datas[0])):
-        perc.append((random.randrange(0, 5)) / 500.0)
-
-    for i in range(100):
-        for data in datas:
-            exac_learn(data)
-
+    random.shuffle(datas)
     f.close()
+
+    init(len(datas[0][1]))
+    for i in range(1000):
+        changed = False
+        for data in datas:
+            if exac_learn(data):
+                changed = True
+        if not changed:
+            break

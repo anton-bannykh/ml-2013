4c37bb602066e116fb396ab9256ddcd0bad62700
Andrew Shulayev <ash.drone@gmail.com>
2013-11-16 19:34:25 +0400

    Simplified main loop code a little

--- andrew.shulayev/cancer_svm/svm_smo.py
+++ andrew.shulayev/cancer_svm/svm_smo.py
@@ -66,21 +66,22 @@ def sequential_minimal_optimization(xs, ys, C, kernel, max_passes=10, tolerance=
         changed = 0
         for i, (x, y) in enumerate(zip(xs, ys)):
             E = target_function(a, xs, ys, b, kernel, x)
-            if (y * E < -tolerance and a[i] < C) or (y * E > tolerance and a[i] > C):
-                # KKT conditions are violated
-                j = randrange(0, n - 1)
-                if j >= i:
-                    j += 1
-                f = lambda x: target_function(a, xs, ys, b, kernel, x)
-                result = optimize_pair((a[i], x, y), (a[j], xs[j], ys[j]), C, f, b, kernel, tolerance)
+            if not ((y * E < -tolerance and a[i] < C) or (y * E > tolerance and a[i] > C)):
+                continue
+            # KKT conditions are violated
+            j = randrange(0, n - 1)
+            if j >= i:
+                j += 1
+            f = lambda x: target_function(a, xs, ys, b, kernel, x)
+            result = optimize_pair((a[i], x, y), (a[j], xs[j], ys[j]), C, f, b, kernel, tolerance)
 
-                if result is None:
-                    # can't optimize this pair
-                    continue
-                a1, a2, b = result
-                a[i] = a1
-                a[j] = a2
-                changed += 1
+            if result is None:
+                # can't optimize this pair
+                continue
+            a1, a2, b = result
+            a[i] = a1
+            a[j] = a2
+            changed += 1
         if changed == 0:
             passes += 1
         else:

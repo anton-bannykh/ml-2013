334f816660bf5471a08f8ea09439845a54882cfc
rabodaber <rabodaber@gmail.com>
2013-11-21 21:38:42 +0400

    logistic regression added

--- andrey.vasin/cancer_common/reader.py
+++ andrey.vasin/cancer_common/reader.py
@@ -30,3 +30,34 @@ def get_data():
         x.append(parameters)
     file.close()
     return shuffle(x, y)
+
+def get_scaled_data():
+    x, y = [], []
+    file = urlopen(URL)
+    for line in file.readlines():
+        input = line.decode('utf-8').strip().split(',')
+
+        if input[1] == 'M':
+            y.append(1.0)
+        else:
+            y.append(-1.0)
+
+        parameters = [float(x) for x in input[2:]]
+        parameters.insert(0, 1.0)
+        parameters = numpy.array(parameters)
+        x.append(parameters)
+    file.close()
+    x_min, x_max = numpy.array([1e10] * len(x[0])), numpy.zeros(len(x[0]))
+    for x_cur in x:
+        for i in range(len(x_cur)):
+            if x_min[i] > x_cur[i]:
+                x_min[i] = x_cur[i]
+            if x_max[i] < x_cur[i]:
+                x_max[i] = x_cur[i]
+    for x_cur in x:
+        for i in range(len(x_cur)):
+            if (x_max[i] != x_min[i]):
+                x_cur[i] = (x_cur[i] - x_min[i]) / (x_max[i] - x_min[i])
+            else:
+                x_cur[i] = 1
+    return shuffle(x, y)
---
+++ andrey.vasin/cancer_logistic_regression/main.py
@@ -0,0 +1,85 @@
+from cancer_common.reader import *
+from cancer_logistic_regression.logistic_regression import *
+
+def get_best_constant(x, y, parts):
+    part_size = int(len(x) / parts)
+    minEout = 1
+    best_const = 1
+
+    for deg in range(0, 10):
+        const = (1.0 / 3.0) ** (deg)
+        print(deg)
+        Eout = 0
+
+        for i in range(parts):
+            check_set_x = x[i * part_size: (i + 1) * part_size]
+            check_set_y = y[i * part_size: (i + 1) * part_size]
+
+            training_set_x = x[:i * part_size] + x[(i + 1) * part_size:]
+            training_set_y = y[:i * part_size] + y[(i + 1) * part_size:]
+
+            w = get_w_logistic_regression(training_set_x, training_set_y, const)
+            prediction = predict_logistic_regression(check_set_x, w)
+
+            misclassified = 0
+            for j in range(len(check_set_x)):
+                if check_set_y[j] != prediction[j]:
+                    misclassified += 1
+            Eout += misclassified / len(check_set_x) / parts
+
+        if Eout < minEout:
+            minEout = Eout
+            best_const = const
+
+    return best_const
+
+TEST_PERCENT = 0.2
+PARTS = 10
+
+x, y = get_scaled_data()
+test_size = int(len(x) * TEST_PERCENT)
+
+check_set_x = x[:test_size]
+check_set_y = y[:test_size]
+
+training_set_x = x[test_size:]
+training_set_y = y[test_size:]
+
+const = get_best_constant(training_set_x, training_set_y, PARTS)
+
+w = get_w_logistic_regression(training_set_x, training_set_y, const)
+prediction = predict_logistic_regression(training_set_x, w)
+
+misclassified = 0
+for j in range(len(training_set_x)):
+    if training_set_y[j] != prediction[j]:
+        misclassified += 1
+Ein = misclassified / len(training_set_x)
+
+prediction = predict_logistic_regression(check_set_x, w)
+
+# 'tp' — true positive
+# 'tn' — true negative
+# 'fp' — false positive
+# 'fn' — false negative
+stats = {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0}
+
+for j in range(len(check_set_x)):
+    if check_set_y[j] == -1:
+        key = 'tn' if prediction[j] == -1 else 'fp'
+        stats[key] += 1
+    if check_set_y[j] == 1:
+        key = 'tp' if prediction[j] == 1 else 'fn'
+        stats[key] += 1
+
+precision = stats['tp'] / (stats['tp'] + stats['fp'])
+recall = stats['tp'] / (stats['tp'] + stats['fn'])
+F1 = 2 * precision * recall / (precision + recall)
+Eout = (stats['fp'] + stats['fn']) / len(x[:test_size])
+
+print("regularization constant = %6.5f" % const)
+print('in sample error         = %6.2f' % (100 * Ein))
+print('out of sample error     = %6.2f' % (100 * Eout))
+print('precision               = %6.2f' % (100 * precision))
+print('recall                  = %6.2f' % (100 * recall))
+print('F1                      = %6.2f' % (100 * F1))

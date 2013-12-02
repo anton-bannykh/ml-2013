4a832cb25bb5d91939ab5275df4bb715e091cb27
tema-popov <artpop92@gmail.com>
2013-12-02 15:58:20 +0400

    Artem Popov: lab 2 and 3 done

--- artem.popov/cancer/perceptron.py
+++ artem.popov/cancer/perceptron.py
@@ -1,31 +1,31 @@
 import numpy
-from cancer.tools import precision, recall, error_rate
+from cancer.tools import precision, recall, error_rate, get_data_set
 from tools import training_and_test_sets
 
 
-def get_answers_and_actual(learning_set, omega):
-    actual = [test.answer for test in learning_set]
-    answers = [numpy.sign(omega.dot(test.features.T)) for test in learning_set]
+def get_answers_and_actual(test_set, omega):
+    actual = [test.answer for test in test_set]
+    answers = [numpy.sign(omega.dot(test.features.T)) for test in test_set]
     return answers, actual
 
 
-def learn_perceptron(learning_set):
+def learn_perceptron(training_set):
     """
-    @type learning_set: list of DataElement
+    @type training_set: list of DataElement
     """
-    omega = numpy.zeros(learning_set[0].features.shape)
+    omega = numpy.zeros(training_set[0].features.shape)
     best_omega = omega.copy()
-    actual = [test.answer for test in learning_set]
-    answers = [numpy.sign(omega.dot(test.features.T)) for test in learning_set]
+    actual = [test.answer for test in training_set]
+    answers = [numpy.sign(omega.dot(test.features.T)) for test in training_set]
     best_error_rate = error_rate(answers, actual)
     for x in xrange(100):
         updates = 0
-        for test in learning_set:
+        for test in training_set:
             if numpy.sign(omega.dot(test.features.T)) != test.answer:
                 updates += 1
                 omega += test.answer * test.features
 
-        answers, actual = get_answers_and_actual(learning_set, omega)
+        answers, actual = get_answers_and_actual(training_set, omega)
         new_error = error_rate(answers, actual)
 
         if new_error < best_error_rate:
@@ -39,14 +39,20 @@ def learn_perceptron(learning_set):
 
 
 def main():
-    training_set, test_set = training_and_test_sets()
-    omega = learn_perceptron(training_set)
-    real_answers = [test.answer for test in test_set]
-    classification_answers = [numpy.sign(omega.dot(test.features.T)) for test in test_set]
-    print "error_rate: %f" % error_rate(classification_answers, real_answers)
-    print "precision: %f" % precision(classification_answers, real_answers)
-    print "recall: %f" % recall(classification_answers, real_answers)
-
+    data_set = get_data_set()
+    error_rates = []
+    for x in xrange(40):
+        print "test %d" % (x + 1)
+        training_set, test_set = training_and_test_sets(data_set)
+        omega = learn_perceptron(training_set)
+        real_answers = [test.answer for test in test_set]
+        classification_answers = [numpy.sign(omega.dot(test.features.T)) for test in test_set]
+        rate = error_rate(classification_answers, real_answers)
+        error_rates.append(rate)
+        print "error_rate: %f" % rate
+        print "precision: %f" % precision(classification_answers, real_answers)
+        print "recall: %f" % recall(classification_answers, real_answers)
+    print "error mean error: %0.5f" % (sum(error_rates) / float(len(error_rates)))
 
 if __name__ == '__main__':
     main()
--- artem.popov/cancer/tools.py
+++ artem.popov/cancer/tools.py
@@ -34,14 +34,26 @@ def load_cancer_dataset():
         return load_data_from_file()
 
 
-def training_and_test_sets():
+def get_data_set():
+    dataset = [DataElement(list) for list in load_cancer_dataset()]
+    l = len(dataset[0].features[0])
+    for i in xrange(l):
+        feature_values = [test.features[0][i] for test in dataset]
+        mean = sum(feature_values) / float(len(feature_values))
+        norm = max(feature_values) - min(feature_values)
+        for test in dataset:
+            test.features[0][i] = (test.features[0][i] - mean) / norm
+    return dataset
+
+
+def training_and_test_sets(dataset):
     """
     @rtype: list of DataElement, list of DataElement
     """
-    dataset = [DataElement(list) for list in load_cancer_dataset()]
-    random.shuffle(dataset)
-    separate_point = int(len(dataset) * 0.8)
-    return dataset[:separate_point], dataset[separate_point:]
+    dataset_ = list(dataset)
+    random.shuffle(dataset_)
+    separate_point = int(len(dataset_) * 0.8)
+    return dataset_[:separate_point], dataset[separate_point:]
 
 
 def error_rate(answers, actual):
@@ -54,6 +66,7 @@ def precision(answers, actual):
     pairs = zip(answers, actual)
     answered = len([1 for pair in pairs if pair[0] == 1])
     good = len([1 for pair in pairs if pair[0] == 1 and pair[1] == 1])
+    if not answered: return 0
     return float(good) / answered
 
 
